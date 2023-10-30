125f781debd72a41aa7ada4fa1676a57ef89de73
najaeseong1 <skwotjd5576@gmail.com>
2023-10-30 15:48:40 +0900

    2023-10-30 리액트 처음 연습

---
+++ src/NoName.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NoName extends React.Component {
+  render() {
+    return (
+      <div className='wrap'>
+        <h1>NoName</h1>
+      </div>
+    );
+  }
+}
+
+export default NoName;
