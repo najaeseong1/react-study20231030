82a8a94d18f49ce484d3c4b63d226d8f95a58cbd
najaeseong1 <skwotjd5576@gmail.com>
2023-10-31 14:36:50 +0900

    2023-10-31 컴포넌트 분할하기 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 const App = () => {
   //지출 항목 객체 배열
@@ -9,37 +9,23 @@ const App = () => {
     {
       title: '바나나',
       price: 2000,
-      date: new Date(2023, 3-1, 23),
+      date: new Date(2023, 3 - 1, 23),
     },
     {
       title: 'BBQ치킨',
       price: 20000,
-      date: new Date(2023, 5-1, 21),
+      date: new Date(2023, 5 - 1, 21),
     },
     {
       title: '도미노피자',
       price: 35000,
-      date: new Date(2023, 7-1, 4),
+      date: new Date(2023, 7 - 1, 4),
     },
   ];
 
   return (
     <>
-      <ExpenseItem
-        title={expenses[0].title}
-        price={expenses[0].price}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        price={expenses[1].price}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        price={expenses[2].price}
-        date={expenses[2].date}
-      />
+      <Expenses items={expenses} />
     </>
   );
 };
