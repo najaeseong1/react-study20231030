17d433ec55c14b11eb5c9af268257c259d65b265
najaeseong1 <skwotjd5576@gmail.com>
2023-11-08 09:35:41 +0900

    2023-11-07 useEffect기초, cleanup함수, useReducer로 다중상태값 중앙관리

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -1,65 +1,75 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './AddUsers.module.css';
 import Card from '../UI/Card';
 import Button from '../UI/Button/Button';
+import ErrorModal from '../UI/Modal/ErrorModal';
 
-const AddUsers = () => {
-  const [userValue, setUserValue] = useState({
-    userName: '',
-    age: '',
-  });
+const AddUsers = ({ onAddUser }) => {
+  // 에러 상태 관리
+  const [error, setError] = useState(null);
 
-  const userNameChangeHandler = (e) => {
-    setUserValue((prevUserValue) => ({
-      ...prevUserValue,
-      userName: e.target.value,
-    }));
-  };
-
-  const ageChangeHandler = (e) => {
-    setUserValue((prevUserValue) => ({
-      ...prevUserValue,
-      age: e.target.value,
-    }));
-  };
+  // input dom 가져오기
+  const nameInput = useRef();
+  const ageInput = useRef();
 
   const userSubmitHandler = (e) => {
     e.preventDefault();
 
-    if (userValue.userName.trim() === '' || userValue.age.trim() === '') {
+    console.log(nameInput.current);
+
+    const username = nameInput.current.value;
+    const age = ageInput.current.value;
+
+    if (username.trim() === '' || age.trim() === '') {
+      setError({
+        title: '유효하지 않은 입력값',
+        message: '입력값은 공백으로 작성하면 안됩니다!',
+      });
       return;
     }
 
-    if (+userValue.age < 1) return;
+    if (+age < 1) {
+      setError({
+        title: '유효하지 않은 나이의 범위',
+        message: '나이는 1 이상의 숫자로 작성해 주세요!',
+      });
+      return;
+    }
 
-    console.log(userValue);
+    onAddUser({ username, age });
 
-    setUserValue({
-      userName: '',
-      age: '',
-    });
+    nameInput.current.value = '';
+    ageInput.current.value = '';
   };
 
   return (
-    <Card className={styles.input}>
-      <form onSubmit={userSubmitHandler}>
-        <label htmlFor='username'>이름</label>
-        <input
-          id='username'
-          type='text'
-          onChange={userNameChangeHandler}
-          value={userValue.userName}
+    // React.Fragment
+    <>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={() => setError(null)}
         />
-        <label htmlFor='age'>나이</label>
-        <input
-          id='age'
-          type='number'
-          onChange={ageChangeHandler}
-          value={userValue.age}
-        />
-        <Button type='submit'>가입하기</Button>
-      </form>
-    </Card>
+      )}
+      <Card className={styles.input}>
+        <form onSubmit={userSubmitHandler}>
+          <label htmlFor='username'>이름</label>
+          <input
+            id='username'
+            type='text'
+            ref={nameInput}
+          />
+          <label htmlFor='age'>나이</label>
+          <input
+            id='age'
+            type='number'
+            ref={ageInput}
+          />
+          <Button type='submit'>가입하기</Button>
+        </form>
+      </Card>
+    </>
   );
 };
 
